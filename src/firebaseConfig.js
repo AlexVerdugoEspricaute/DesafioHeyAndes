78634a05a6746d4483f73df43b263b10f1dab612
AlexVerdugo <alex.verdugo.-@hotmail.com>
2024-06-24 12:23:22 -0400

    terminado

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAN4aPtuBukguunlrRCBzSQDyArDWZ5Ffw",
+    authDomain: "desafio1-37966.firebaseapp.com",
+    projectId: "desafio1-37966",
+    storageBucket: "desafio1-37966.appspot.com",
+    messagingSenderId: "594110836737",
+    appId: "1:594110836737:web:8239a0c4dabdad9bba5000"
+}; 
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import store from './redux/store';
 import './index.css';
+import './firebaseConfig'; 
+
 
 ReactDOM.render(
   <Provider store={store}>
